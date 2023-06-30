8eca35a14549f84890fcebb2dc42b96e52cb4b0a
Illia Rakitov <84653505+iirakitov@users.noreply.github.com>
2023-06-30 13:18:23 +0300

    unresolved MainActivity error correct

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import com.example.numbersapp.R
+
 
 class MainActivity : AppCompatActivity() {
 
